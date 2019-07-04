845bad86a06c90f97b821a289b8214fdae8724e8
asche98 <super-elena11@mail.ru>
2019-07-05 00:42:39 +0500

    edit search

--- src/components/transactionTable.js
+++ src/components/transactionTable.js
@@ -15,14 +15,14 @@ class transactionTable extends React.Component{
     var statusStyle;
     var i=0;
     return(
-      <div class="container">
-      <div class="col-lg-12"><h3 className="tableHeader">Таблица транзакций</h3></div>
-      <div class="col-lg-12">
-  <input class="form-control"
-         placeholder="Введите значение для поиска по таблице (например, Jack Black или Red Sauce)" onChange={this.onchange}/>
-         <br/>
+      <div className="container">
+      <div className="col-lg-12"><h3 className="tableHeader">Таблица транзакций</h3></div>
+      <div className="col-lg-12">
+      <input type="search" className="form-control" placeholder="Введите значение для поиска по таблице (например, Jack Black или Red Sauce)" onChange={this.onchange}/>
+      <small className="form-text ">Фильтрация производится по полям "Название проекта", "Платёжный метод", "Статус", "Пользователь" и "Заказ". Поиск не чувствителен к регистру.</small><br/>
       <table className="table table-striped table-bordered">
       <thead>
+      <tr>
       <th>№</th>
       <th>Название проекта</th>
       <th>Платёжный метод</th>
@@ -31,17 +31,11 @@ class transactionTable extends React.Component{
       <th>Пользователь</th>
       <th>Оплаченные средства</th>
       <th>Заказ</th>
+      </tr>
       </thead>
       <tbody>
       {
         data.map((data) => {
-          var s1=data.transaction.project.name.toLowerCase().indexOf(search.toLowerCase())!=-1;
-          var s2=data.transaction.payment_method.name.toLowerCase().indexOf(search.toLowerCase())!=-1;
-          var s3=data.transaction.status.toLowerCase().indexOf(search.toLowerCase())!=-1;
-          var s4=data.user.id.toLowerCase().indexOf(search.toLowerCase())!=-1;
-          var s5=(data.user.email)&&data.user.email.toLowerCase().indexOf(search.toLowerCase())!=-1;
-          if(search!="" && !(s1 || s2 ||s3 || s4 || s5)) return null;
-          i++;
           refundReason='';
           purchaseInfo='';
           switch(data.transaction.status){
@@ -61,6 +55,16 @@ class transactionTable extends React.Component{
           if(data.purchase.pin_codes.amount!=null) purchaseInfo+="Пин-коды: "+data.purchase.pin_codes.content+" \n";
           if(data.purchase.simple_checkout.amount!=0) purchaseInfo+="Simple checkout: "+data.purchase.simple_checkout.amount+" "+data.purchase.simple_checkout.currency+" \n";
           if(data.purchase.subscription.name!=null) purchaseInfo+="Подписки: "+data.purchase.subscription.name+" ";
+          //for searching
+          var s1=data.transaction.project.name.toLowerCase().indexOf(search.toLowerCase())!=-1;
+          var s2=data.transaction.payment_method.name.toLowerCase().indexOf(search.toLowerCase())!=-1;
+          var s3=data.transaction.status.toLowerCase().indexOf(search.toLowerCase())!=-1;
+          var s4=data.user.id.toLowerCase().indexOf(search.toLowerCase())!=-1;
+          var s5=(data.user.email)&&data.user.email.toLowerCase().indexOf(search.toLowerCase())!=-1;
+          var s6=refundReason.toLowerCase().indexOf(search.toLowerCase())!=-1;
+          var s7=purchaseInfo.toLowerCase().indexOf(search.toLowerCase())!=-1;
+          if(search!="" && !(s1 || s2 ||s3 || s4 || s5 || s6 || s7)) return null;
+          i++;
         var myDate=new Date(data.transaction.transfer_date);
         return( <tr>
         <td>{i}</td>
